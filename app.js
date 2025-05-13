e72e3cbfbdcc0808f14b4b4957f9e68573f89874
VoilaSecari <lifeaslaurafilip@gmail.com>
2025-05-13 13:29:31 +0200

    aggiustamento routing finale

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// importo data
+let { posts } = require("./data/posts.js");
+
 //importo router
 const blogRouter = require("./routers/posts.js");
 
--- routers/posts.js
+++ routers/posts.js
@@ -2,7 +2,50 @@
 const express = require("express");
 const router = express.Router();
 
+//importo data
+let { posts } = require("../data/posts.js");
+
 // definizione delle rotte
+
+// INDEX (base, indice insomma)
+// .get
+router.get("", (req, res) => {
+  res.json("Lettura sul blog dei posts.");
+});
+
+// SHOW (QUI PER LO SPECIFICO POST)
+// .get
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  res.json("Lettura sul blog del post " + id);
+});
+
+// STORE (qui si crea nuovo post)
+// .post
+router.post("", (req, res) => {
+  res.json("Creazione sul blog di un nuovo post.");
+});
+
+// UPDATE (qui si sostituisce un post del blog)
+// .put
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  res.json("Sostituzione sul blog del post " + id);
+});
+
+// MODIFY (qui si modifica un post del blog)
+// .patch
+router.patch("/:id", (req, res) => {
+  const id = req.params.id;
+  res.json("Modifica sul blog del post " + id);
+});
+
+// DESTROY (con questo si elimina un post)
+// .delete
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  res.json("Eliminazione sul blog del post " + id);
+});
 
 // export router
 
