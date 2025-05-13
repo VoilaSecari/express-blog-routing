dd209dea9ac2510d3bf31019bdb8f2942198a793
VoilaSecari <lifeaslaurafilip@gmail.com>
2025-05-13 13:17:22 +0200

    inizializzazione rel routing

--- app.js
+++ app.js
@@ -4,53 +4,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//importo router
+const blogRouter = require("./routers/posts.js");
+
+// routers
+app.use("/posts", blogRouter);
+
 //implementazione servizio public
 
 app.use(express.static("public"));
-
-//root per salutare
-
-app.get("/", (req, res) => {
-  res.json({ message: "Ciao, questo è il mio blog!" });
-});
-
-// url per i post nella bacheca
-
-app.get("/bacheca", (req, res) => {
-  const posts = [
-    {
-      name: "Ciambellone",
-      description: "un ciambellone",
-      img: "/img/ciambellone.jpeg",
-      tags: "ciambella",
-    },
-    {
-      name: "Cracker alla Barbabietola",
-      description: "Dei crackers alla barbabietola",
-      img: "/img/cracker_barbabietola.jpeg",
-      tags: "cracker, barbabietola",
-    },
-    {
-      name: "Pane Fritto Dolce",
-      description: "Del pane fritto dolce",
-      img: "/img/pane_fritto_dolce.jpeg",
-      tags: "pane, fritto, dolce",
-    },
-    {
-      name: "Pasta alle Barbabietole",
-      description: "Della pasta alla barbabietola",
-      img: "/img/pasta_barbabietola.jpeg",
-      tags: "pasta, barbabietola",
-    },
-    {
-      name: "Torta Paesana",
-      description: "Della Torta paesana",
-      img: "/img/torta_paesana.jpeg",
-      tags: "torta, paesana",
-    },
-  ];
-  res.json(posts);
-});
 
 // ascolto
 
---
+++ routers/posts.js
@@ -0,0 +1,9 @@
+// baseline
+const express = require("express");
+const router = express.Router();
+
+// definizione delle rotte
+
+// export router
+
+module.exports = router;
